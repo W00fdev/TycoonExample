42243f67f9354fdb95e25b7bf28e57239c747a75
dmitry <w00f.devs@gmail.com>
2025-03-29 12:30:47 +0300

    refactor: fix camera script namings

--- Weapon Tycoon/Assets/_Project/Scripts/Components/Character/CameraMovement.cs
+++ Weapon Tycoon/Assets/_Project/Scripts/Components/Character/CameraMovement.cs
@@ -1,70 +1,58 @@
 using _Project.Scripts.Infrastructure.ScriptableEvents;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace _Project.Scripts.Components.Character
 {
     public class CameraMovement : MonoBehaviour
     {
         [SerializeField] private Camera _camera;
+        [SerializeField] private Transform _player;
         
-        public float sensitivity = 5f;
-        public Vector2 cameraLimit = new Vector2(-45, 40);
+        [SerializeField] private float _sensitivity = 5f;
+        [SerializeField] private Vector2 _cameraLimit = new Vector2(-45, 40);
 
-        float mouseX;
-        float mouseY;
-        public float offsetDistanceY;
-        public float offsetDistanceX = 3;
-        public float distance;
+        [SerializeField] private float _offsetDistanceY;
+        [SerializeField] private float _offsetDistanceX = 3;
+        [SerializeField] private float _distance;
 
         [SerializeField] private LayerMask _allExceptPlayer;
         [SerializeField] private float _smoothingStep;
         
-        private Transform player;
-        private RaycastHit[] _anyCollider;
+        private float _mouseX;
+        private float _mouseY;
         private float _offsetZ;
-        private bool _movementAllowed;
-
-        void Start()
-        {
-            _anyCollider = new RaycastHit[1];
-            player = GameObject.FindWithTag("Player").transform;
-
-            _movementAllowed = true;
-        }
+        private bool _movementAllowed = true;
 
         // Context Invokation
-        public void StopCameraMovement(Empty _)
-        {
-            _movementAllowed = false;
-        }
-        
+        public void StopCameraMovement(Empty _) 
+            => _movementAllowed = false;
+
         // Context Invokation
-        public void ResumeCameraMovement(Empty _)
-        {
-            _movementAllowed = true;
-        }
-        
+        public void ResumeCameraMovement(Empty _) 
+            => _movementAllowed = true;
+
         void LateUpdate()
         {
             if (!_movementAllowed)
                 return;
             
-            mouseX += Input.GetAxis("Mouse X") * sensitivity;
-            mouseY += Input.GetAxis("Mouse Y") * sensitivity;
-            mouseY = Mathf.Clamp(mouseY, cameraLimit.x, cameraLimit.y);
+            _mouseX += Input.GetAxis("Mouse X") * _sensitivity;
+            _mouseY += Input.GetAxis("Mouse Y") * _sensitivity;
+            _mouseY = Mathf.Clamp(_mouseY, _cameraLimit.x, _cameraLimit.y);
 
-            _camera.transform.rotation = Quaternion.Euler(-mouseY, mouseX, 0);
+            _camera.transform.rotation = Quaternion.Euler(-_mouseY, _mouseX, 0);
 
-            var position = _camera.transform.rotation * new Vector3(offsetDistanceX, 0, -distance);
-            position += player.position;
-            position += Vector3.up * offsetDistanceY;
+            var position = _camera.transform.rotation * new Vector3(_offsetDistanceX, 0, -_distance);
+            position += _player.position;
+            position += Vector3.up * _offsetDistanceY;
             
-            Ray backSpaceCamera = new Ray(player.position, -_camera.transform.forward);
-            float distanceToCamera = Vector3.Distance(player.position, _camera.transform.position);
+            Ray backSpaceCamera = new Ray(_player.position, -_camera.transform.forward);
+            float distanceToCamera = Vector3.Distance(_player.position, _camera.transform.position);
             float distanceToWall = distanceToCamera;
             
-            if (Physics.RaycastNonAlloc(backSpaceCamera, _anyCollider, distanceToCamera + 0.1f, _allExceptPlayer.value) > 0)
-                distanceToWall = Vector3.Distance(player.position, _anyCollider[0].point);
+            if (Physics.Raycast(backSpaceCamera, out var hit, distanceToCamera + 0.1f, _allExceptPlayer.value))
+                distanceToWall = Vector3.Distance(_player.position, hit.point);
 
             _offsetZ = Mathf.SmoothStep(_offsetZ, Mathf.Abs(distanceToWall - distanceToCamera), _smoothingStep * Time.deltaTime);
             if (distanceToWall <= 10f)
